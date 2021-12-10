1498df9735f8639f1a9648e659a903ba1bfdcba7
pravin86n <pravin86n@gmail.com>
2021-12-10 10:58:36 -0500

    user authentication and verification

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,18 +1,21 @@
 import { useContext } from "react";
 import { UserContext } from "../../context";
+import UserRoute from "../../components/routes/UserRoute";
 
 const Home = () => {
   // this will give you access to the global state set in UserContext.
   const [state, setState] = useContext(UserContext);
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <h1 className="display-1 text-center">Dashboard Page</h1>
+    <UserRoute>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1 className="display-1 text-center">Dashboard Page</h1>
+          </div>
         </div>
       </div>
-    </div>
+    </UserRoute>
   );
 };
 
