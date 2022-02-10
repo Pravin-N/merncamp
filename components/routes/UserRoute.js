c95878ccae5384456c4b0db973a7ff619f618438
pravin86n <pravin86n@gmail.com>
2022-02-10 06:09:06 -0500

    formatted file

--- components/routes/UserRoute.js
+++ components/routes/UserRoute.js
@@ -7,36 +7,36 @@ import { UserContext } from "../../context";
 const UserRoute = ({ children }) => {
   const [ok, setOk] = useState(false);
   const router = useRouter();
-  const [state] = useContext(UserContext)
+  const [state] = useContext(UserContext);
 
   useEffect(() => {
-    if(state && state.token) getCurrentUser();
+    if (state && state.token) getCurrentUser();
   }, [state && state.token]);
 
   const getCurrentUser = async () => {
     try {
-      const { data } = await axios.get(
-        `/current-user`
-      );
-      if (data.ok) {setOk(true)};
+      const { data } = await axios.get(`/current-user`);
+      if (data.ok) {
+        setOk(true);
+      }
     } catch (err) {
       router.push("/login");
     }
   };
 
-  process.browser && state === null && setTimeout(() => {
-    getCurrentUser();
-  }, 1000)
-  
+  process.browser &&
+    state === null &&
+    setTimeout(() => {
+      getCurrentUser();
+    }, 1000);
+
   return !ok ? (
     <SyncOutlined
       spin
       className="d-flex justify-content-center display-1 text-primary p-5"
     />
   ) : (
-    <>
-    { children }
-    </>
+    <>{children}</>
   );
 };
 
